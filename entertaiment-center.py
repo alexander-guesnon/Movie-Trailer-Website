08a2807bc5b7a6737b6defd0985cbd7649794443
alexgooz <alexgooz92fs@gmail.com>
2017-07-23 21:47:16 -0700

    orginizing datat and set  up
    I am organizing all of my favorite movies so it will be easy to throw
    them into the movie object.

--- entertaiment-center.py
+++ entertaiment-center.py
@@ -1,10 +1,47 @@
 #TODO edit fresh_tomatoes css and html to be more personal
 #TODO prefect the code to follow googles style guide
-#main somthing
+
 #take in class movie and fresh_tomatoes
+import media
+import fresh_tomatoes
 
 # make 6 of my favorit movies acording to the constructor
-
+#Punisher
+'''
+Title: The Punisher (1989)
+Box Art: https://upload.wikimedia.org/wikipedia/en/c/c7/Punisher89poster.jpg
+Trailer: https://www.youtube.com/watch?v=umYvv7K4Z_I
+'''
+#goodfellas
+'''
+Title: Goodfellas
+Box Art: https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg
+Trailer: https://www.youtube.com/watch?v=2ilzidi_J8Q
+'''
+#Hackers
+'''
+Title: Hackers (1995)
+Box Art: https://upload.wikimedia.org/wikipedia/en/6/67/Hackersposter.jpg
+Trailer: https://www.youtube.com/watch?v=Rn2cf_wJ4f4
+'''
+#Jaws
+'''
+Title: Jaws
+Box Art: https://upload.wikimedia.org/wikipedia/en/e/eb/JAWS_Movie_poster.jpg
+Trailer: https://www.youtube.com/watch?v=U1fu_sA7XhE
+'''
+#The Godfather
+'''
+Title: The Godfather
+Box Art: https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg
+Trailer: https://www.youtube.com/watch?v=sY1S34973zA
+'''
+#The Matrix
+'''
+Title: The Matrix
+Box Art: https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg
+Trailer: https://www.youtube.com/watch?v=vKQi3bBA1y8
+'''
 # shove all 6 of thos movies into a array
 
 #send that array to the fresh_tomatoes.py through open_movies_page()
