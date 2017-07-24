be23c667af667fc9a68a0f4d78048ff9e37dfa9d
alexgooz <alexgooz92fs@gmail.com>
2017-07-23 22:13:39 -0700

    working websit
    I put all of the movie data in individual movie instances.
    Then I took those instances and shoved them into an array.
    Then the movie array will be sent to fresh tomatoes through the
    open_movie_page() and that will launch the website.
    I had to change some variables on the fresh_tomatos so it would run
    
    now clearn up time.

--- entertaiment-center.py
+++ entertaiment-center.py
@@ -5,43 +5,41 @@
 import media
 import fresh_tomatoes
 
+# shove all 6 of thos movies into a array
+movie_list = [
+media.Movie("The Punisher (1989)"," https://upload.wikimedia.org/wikipedia/en/c/c7/Punisher89poster.jpg","https://www.youtube.com/watch?v=umYvv7K4Z_I"),
+media.Movie("Goodfellas", "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg", "https://www.youtube.com/watch?v=2ilzidi_J8Q"),
+media.Movie("Hackers (1995)", "https://upload.wikimedia.org/wikipedia/en/6/67/Hackersposter.jpg", "https://www.youtube.com/watch?v=Rn2cf_wJ4f4"),
+media.Movie("Jaws", "https://upload.wikimedia.org/wikipedia/en/e/eb/JAWS_Movie_poster.jpg", "https://www.youtube.com/watch?v=U1fu_sA7XhE"),
+media.Movie("The Godfather", "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg", "https://www.youtube.com/watch?v=sY1S34973zA"),
+media.Movie("The Matrix", "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg", "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+]
+
 # make 6 of my favorit movies acording to the constructor
+
 #Punisher
-'''
-Title: The Punisher (1989)
-Box Art: https://upload.wikimedia.org/wikipedia/en/c/c7/Punisher89poster.jpg
-Trailer: https://www.youtube.com/watch?v=umYvv7K4Z_I
-'''
+
 #goodfellas
 '''
-Title: Goodfellas
-Box Art: https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg
-Trailer: https://www.youtube.com/watch?v=2ilzidi_J8Q
+
 '''
 #Hackers
 '''
-Title: Hackers (1995)
-Box Art: https://upload.wikimedia.org/wikipedia/en/6/67/Hackersposter.jpg
-Trailer: https://www.youtube.com/watch?v=Rn2cf_wJ4f4
+
 '''
 #Jaws
 '''
-Title: Jaws
-Box Art: https://upload.wikimedia.org/wikipedia/en/e/eb/JAWS_Movie_poster.jpg
-Trailer: https://www.youtube.com/watch?v=U1fu_sA7XhE
+
 '''
 #The Godfather
 '''
-Title: The Godfather
-Box Art: https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg
-Trailer: https://www.youtube.com/watch?v=sY1S34973zA
+
 '''
 #The Matrix
 '''
-Title: The Matrix
-Box Art: https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg
-Trailer: https://www.youtube.com/watch?v=vKQi3bBA1y8
+
 '''
-# shove all 6 of thos movies into a array
 
 #send that array to the fresh_tomatoes.py through open_movies_page()
+# fresh toamatos takes the movie list and turns it into a web page.
+fresh_tomatoes.open_movies_page(movie_list)
