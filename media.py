054a3119504799b021a352529715c2b532e8c76b
alexgooz <alexgooz92fs@gmail.com>
2017-07-23 21:25:29 -0700

    class is constructed
    The class is now constructed now on to the show_trailer

--- media.py
+++ media.py
@@ -2,8 +2,17 @@
 class Movie:
     #documention
     """
-    test
+    This Object will store the title, box art, and movie trailer upon creation.
+
+    If you uses the show_trailer function it will lanuch trailer in a web page.
     """
     #constructor
-    def __init__(self):
+    def __init__(self,movie_title,movie_box_art, movie_trailer):
+        self.title=movie_title
+        self.box_art=movie_box_art
+        self.trailer=movie_trailer
+
     #show_trailer
+    def show_trailer(self):
+        
+#end of movie class
